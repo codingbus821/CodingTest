65fe36b363f18af33982135496f0c022d9a624cb
kimbyeongsoo <kbsu147@naver.com>
2020-07-02 18:34:00 +0900

    Initial Commit

---
+++ CodingTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CodingTest
+//
+//  Created by 김병수 on 2020/07/02.
+//  Copyright © 2020 coconut. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
