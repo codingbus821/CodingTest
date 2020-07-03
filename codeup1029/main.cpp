20c6be80b7922708fca78c53b8b115916b7743cc
kimbyeongsoo <kbsu147@naver.com>
2020-07-03 21:03:14 +0900

    first commit

---
+++ codeup1029/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  codeup1029
+//
+//  Created by 김병수 on 2020/07/03.
+//  Copyright © 2020 coconut. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    double d;
+    scanf("%lf", &d); // double(long float) 형식으로 입력
+    printf("%.11lf", d);
+
+}
