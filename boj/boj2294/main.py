4a9c8bc4cd8ef394efb16f92770837f13908da03
kimbyeongsu <kbsu147@naver.com>
2025-02-05 16:26:57 +0900

    3 12 1 5 8 반례 획인

--- boj/boj2294/main.py
+++ boj/boj2294/main.py
@@ -1,6 +1,33 @@
 n, k = map(int, input().split())
 
-n_list = []
+n_set = set()
+
+ret = 111111
 
 for _ in range(n):
-    n_list.append(int(input()))
+    n_set.add(int(input()))
+
+n_list = list(n_set)
+
+n_list.sort(reverse=True)
+
+for i in range(len(n_list)):
+    tmp_sum = 0
+    de = k
+    if de == 0 or n_list[i] == 0:
+        break
+    if de / n_list[i] > 0:
+        tmp_sum += de // n_list[i]
+        de = de % n_list[i]
+        for j in range(i+1, len(n_list)):
+            if de == 0 or n_list[j] == 0:
+                break
+            if de / n_list[j] > 0:
+                tmp_sum += de // n_list[j]
+                de = de % n_list[j]
+    if de == 0 and tmp_sum < ret:
+        ret = tmp_sum
+if ret == 111111:
+    print(-1)
+else: 
+    print(ret)
