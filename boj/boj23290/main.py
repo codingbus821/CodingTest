38bbe675992506d1ab34e9d2351a406350e8fafc
kimbyeongsu <kbsu147@naver.com>
2025-04-08 13:08:21 +0900

    boj23290

--- boj/boj23290/main.py
+++ boj/boj23290/main.py
@@ -7,8 +7,9 @@ M, S = map(int, sys.stdin.readline().split())
 fish = [[[] for _ in range(5)] for _ in range(5)]
 smell_board = [[0]*5 for _ in range(5)]
 
-fish_dy=[0,0,-1,-1,-1,0,1,1,1]
-fish_dx=[0,-1,-1,0,1,1,1,0,-1]
+fish_dx = [0, -1, -1, 0, 1, 1, 1, 0, -1]
+fish_dy = [0, 0, -1, -1, -1, 0, 1, 1, 1]
+
 s_dy=[-1,0,1,0]
 s_dx=[0,-1,0,1]
 
@@ -27,23 +28,23 @@ def move(tmp):
         for j in range(1,5):
             while tmp[i][j]:
                 d = tmp[i][j].pop()
-                for k in range(d, d-8, -1):
-                    k = k % 8
-                    ny,nx = i+fish_dy[k], j+fish_dx[k]
+                for k in range(8):
+                    nd = (d - k - 1) % 8 + 1
+                    ny,nx = i+fish_dy[nd], j+fish_dx[nd]
                     if 1<=nx<=4 and 1<=ny<=4 and (ny,nx) != (sy,sx) and smell_board[ny][nx] == 0:
-                        ret[ny][nx].append(k)
+                        ret[ny][nx].append(nd)
                         break
                 else:
                     ret[i][j].append(d)
     return ret
 
 
-def dfs(y,x,m_cnt,total,visit):
+def dfs(y,x,m_cnt,total,visit,tmp):
     
-    global m_m_list, m_total,sy,sx
-    
+    global m_total, m_m_list,sy,sx
     if m_cnt == 3:
         if total > m_total:
+            sy,sx = y,x
             m_total = total
             m_m_list = visit[:]
         return
@@ -53,21 +54,21 @@ def dfs(y,x,m_cnt,total,visit):
         if 1<=nx<=4 and 1<=ny<=4:
             if (ny,nx) not in visit:
                 visit.append((ny,nx))
-                dfs(ny,nx,m_cnt+1,total+len(tmp[ny][nx]),visit)
+                dfs(ny,nx,m_cnt+1,total+len(tmp[ny][nx]),visit,tmp)
                 visit.pop()
             else:
-                dfs(ny,nx,m_cnt+1,total,visit)
+                dfs(ny,nx,m_cnt+1,total,visit,tmp)
         
 
 for i in range(S):
     m_total = -1
     m_m_list = list()
     # print(i,"----------")
-    tmp = copy.deepcopy(fish)
-    tmp = move(tmp)
-    dfs(sy,sx,0,0,list())
+    backup = copy.deepcopy(fish)        # Step 1: backup for copy
+    tmp = move(copy.deepcopy(fish))     # Step 2: move
+    dfs(sy,sx,0,0,list(),tmp)
     for y,x in m_m_list:
-        sy, sx = y, x
+        # sy, sx = y, x
         if tmp[y][x]:
             tmp[y][x] = []
             smell_board[y][x] = 3
@@ -77,9 +78,12 @@ for i in range(S):
         for j in range(1,5):
             if smell_board[i][j] > 0:
                 smell_board[i][j] -= 1
-    for i in range(1,5):
-        for j in range(1,5):
-            fish[i][j] += tmp[i][j]
+    for i in range(1, 5):
+        for j in range(1, 5):
+            fish[i][j] = tmp[i][j][:]
+    for i in range(1, 5):
+        for j in range(1, 5):
+            fish[i][j].extend(backup[i][j])
     # fish = copy.deepcopy(tmp)
     # print(fish)
 
